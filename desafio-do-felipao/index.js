7325878b50af21919dc486ff81542e52c7d9a955
swetonyancelmo <167218398+swetonyancelmo@users.noreply.github.com>
2024-12-27 21:01:12 -0300

    Add files via upload

---
+++ desafio-do-felipao/index.js
@@ -0,0 +1,31 @@
+// ------DESAFIO DO FELIPÃO------
+
+let heroi = "Calvice do Felipe"
+let xp = 9000
+let nivel = ""
+
+if (xp <= 1000){
+  nivel = "Ferro"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+} else if (xp <= 2000){
+  nivel = "Bronze"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+} else if (xp <= 3000) {
+  nivel = "Prata"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+} else if (xp <= 4000) {
+  nivel = "Ouro"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+} else if (xp <= 5000) {
+  nivel = "Platina"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+} else if (xp <= 6000) {
+  nivel = "Ascendente"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+} else if (xp <= 7000) {
+  nivel = "Imortal"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+} else {
+  nivel = "Radiante"
+  console.log(`O herói do nome ${heroi} está no nível ${nivel}`)
+}
